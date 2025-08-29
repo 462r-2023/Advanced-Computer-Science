a7abf93455e50f32651dc915fe3d396120a3976c
462r-2023 <147111421+462r-2023@users.noreply.github.com>
2025-08-29 15:00:23 -0700

    Unit 0 Lesson 2
    Added Zombie.java

---
+++ Unit Lessons/U0D2/Demo.java
@@ -0,0 +1,12 @@
+// Class -> a set of attributes and behaviors
+public class Demo {
+    public static void main(String[] args) {
+        Demo sample = new Demo();
+
+        System.out.println(sample.havingFun());
+    }
+
+    public String havingFun() {
+        return "I'm having fun!";
+    }
+}
