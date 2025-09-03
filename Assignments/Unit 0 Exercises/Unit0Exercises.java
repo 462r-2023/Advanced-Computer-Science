fa4bdca2eeb79a5012e363474a8785da7bbd2b6b
462r-2023 <147111421+462r-2023@users.noreply.github.com>
2025-09-03 12:57:01 -0700

    Finished Unit 0 Exercises
    Finished all the methods

---
+++ Assignments/Unit 0 Exercises/Unit0Exercises.java
@@ -0,0 +1,45 @@
+// Solution: Unit 0 Exercises
+public class Unit0Exercises {
+
+
+    public static void main(String[] args) {
+        Unit0Exercises obj = new Unit0Exercises();
+        System.out.println();
+
+        // testing sleepIn() method
+        System.out.println(obj.sleepIn(false, false)); // output: true
+        // sleepIn(true, false) -> false
+        // sleepIn(false, true) -> true
+
+        // testing diff21() method
+        System.out.println(obj.diff21(19)); // ouput: 2
+        System.out.println(obj.diff21(22)); // ouput: 2
+        System.out.println(obj.diff21(21)); // ouput: 0
+        // diff21(10) -> 11
+        // diff21(21) -> 0
+
+        // testing flooringCalculator() method
+        System.out.println(obj.flooringCalculator(500, 2.5)); // output: 1250.0
+        // flooringCalculator(0, 3.0) -> 0.0
+        // flooringCalculator(100, -1.5) -> 0.0
+
+    }
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+    public int diff21(int n) {
+        if (n > 21) {
+            return 2 * (n - 21);
+        }
+        return 21 - n;
+
+        // return (int) Math.abs((n - 21) * 3 + (int) Math.abs(n - 21)) / 2;
+    }
+
+    public double flooringCalculator(int sqft, double pricePerSqft) {
+        return Math.max(sqft * pricePerSqft, 0.0);
+    }
+
+}
