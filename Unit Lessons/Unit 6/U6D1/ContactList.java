fcd0c44bf9b70fdcb107a3989f05db2e1223699a
462r-2023 <147111421+462r-2023@users.noreply.github.com>
2026-04-09 14:11:02 -0700

    Create ContactList.java

---
+++ Unit Lessons/Unit 6/U6D1/ContactList.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.AbstractList;
+
+public class ContactList extends AbstractList {
+
+    // instance variable
+    private ArrayList<String> contactList;
+
+    // constructor
+    // to-do: initializes an empty contact list
+    public ContactList() {
+    }
+
+    // methods
+
+    // to-do: findInsertLocation(String name)
+    /**
+     * returns the location in the contact list where the name should go to keep the
+     * list
+     * alphabetized
+     */
+    private int findInsertLocation(String name) {
+        // You can use a sequential search here. But for a Stretch Challenge, try to do
+        // a binary search.
+        return 0;
+    }
+
+    // to-do: add(String name)
+    /**
+     * adds a name to the contact list so that the list remains alphabetized, it
+     * prints out which
+     * name is being added, also the method prevents duplicate names from being
+     * added
+     */
+    public boolean add(String name) {
+        System.out.println("+ Adding " + name);
+        return false;
+    }
+
+    // to-do: add(ArrayList<String> names)
+    /* this method adds a list of names to the contact list */
+    public void add(ArrayList<String> names) {
+
+    }
+
+    // to-do: remove(String name)
+    /** removes name from the contact list and keeps list alphabetized */
+    public boolean remove(String name) {
+        System.out.println("- Removing " + name);
+        return false;
+    }
+
+    // to-do: remove(ArrayList<String> names)
+    /* this method removes a list of names from the contact list */
+    public void remove(ArrayList<String> names) {
+
+    }
+
+    /** returns a String containing all of the words in list */
+    public String toString() {
+        return "Contact List: " + contactList.toString();
+    }
+
+    // to-do: get(int index)
+    /** returns the name at the specified index */
+    public String get(int index) {
+        return null;
+    }
+
+    // to-do: size()
+    /** returns the number of names in the contact list */
+    public int size() {
+        return 0;
+    }
+
+    // to-do: clear()
+    /** removes all names from the contact list */
+    public void clear() {
+        System.out.println("Clearing the contact list");
+    }
+
+}
